036bb913017b0cf8c7166e76f4c372a21a765f3c
Tacəddin Əsədzadə <tcddnesedzade@fmail.com>
2024-10-04 15:53:45 +0400

    task041024

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿//task1
+//int[] array = { 17, 4, 2, 7, };
+//int min = array[0];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (min > array[i])
+//    {
+//        min = array[i];
+//    }
+//}
+//Console.WriteLine(min);
+
+
+
+//task2
+
+//int[] array = { 12, 3, 5, 67, 8 };
+//int sum = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+//    sum += array[i];
+//}
+//Console.WriteLine(sum);
+
+
+
+//task3
+//string[] array = { "Allahverdi", "Taceddin", "Emil", "sabir", "code academy" };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (
+//    {
+//        Console.WriteLine(array[i]);
+//    }
+//}
+
+
